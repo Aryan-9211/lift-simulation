150a191ec5d312bb9b6e49408ff8514d21e20267
Aryan-9211 <71491341+Aryan-9211@users.noreply.github.com>
2021-08-31 10:52:40 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+document.querySelector(".btn1").addEventListener("click", function(){
+    document.querySelector(".lift").style.marginTop = "-70px";
+});
+
+document.querySelector(".btn2").addEventListener("click", function(){
+    document.querySelector(".lift").style.marginTop = "90px";
+})
+
+document.querySelector(".btn3").addEventListener("click", function(){
+    document.querySelector(".lift").style.marginTop = "90px";
+})
+
+document.querySelector(".btn4").addEventListener("click", function(){
+    document.querySelector(".lift").style.marginTop = "230px";
+})
+
+document.querySelector(".btn5").addEventListener("click", function(){
+    document.querySelector(".lift").style.marginTop = "230px";
+})
+
+document.querySelector(".btn6").addEventListener("click", function(){
+    document.querySelector(".lift").style.marginTop = "380px";
+})
+
+
