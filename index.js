07e7fde08807fa94ada26c331fc9ef7888627a51
Aryan <manglaaryan178@gmail.com>
2021-10-13 18:32:29 +0530

    a

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ document.querySelector(".btn1").addEventListener("click", function(){
     document.querySelector(".lift").style.marginTop = "-70px";
 });
 
+
 document.querySelector(".btn2").addEventListener("click", function(){
     document.querySelector(".lift").style.marginTop = "90px";
 })
@@ -22,5 +23,59 @@ document.querySelector(".btn5").addEventListener("click", function(){
 document.querySelector(".btn6").addEventListener("click", function(){
     document.querySelector(".lift").style.marginTop = "380px";
 })
+
+document.querySelector(".btn7").addEventListener("click", function(){
+    document.querySelector(".lift").style.marginTop = "380px";
+})
+
+document.querySelector(".btn8").addEventListener("click", function(){
+    document.querySelector(".lift").style.marginTop = "530px";
+})
+
+document.querySelector(".btn9").addEventListener("click", function(){
+    document.querySelector(".lift").style.marginTop = "530px";
+})
+
+document.querySelector(".btn10").addEventListener("click", function(){
+    document.querySelector(".lift").style.marginTop = "680px";
+})
+
+document.querySelector(".btn11").addEventListener("click", function(){
+    document.querySelector(".lift").style.marginTop = "680px";
+})
+
+document.querySelector(".btn12").addEventListener("click", function(){
+    document.querySelector(".lift").style.marginTop = "820px";
+});
+
+document.querySelector(".btn13").addEventListener("click", function(){
+    document.querySelector(".lift").style.marginTop = "820px";
+})
+
+document.querySelector(".btn14").addEventListener("click", function(){
+    document.querySelector(".lift").style.marginTop = "970px";
+})
+
+document.querySelector(".btn15").addEventListener("click", function(){
+    document.querySelector(".lift").style.marginTop = "970px";
+})
+
+document.querySelector(".btn16").addEventListener("click", function(){
+    document.querySelector(".lift").style.marginTop = "1120px";
+})
+
+document.querySelector(".btn17").addEventListener("click", function(){
+    document.querySelector(".lift").style.marginTop = "1120px";
+})
+
+document.querySelector(".btn18").addEventListener("click", function(){
+    document.querySelector(".lift").style.marginTop = "1270px";
+})
+
+
+
+
+
+
 
 
