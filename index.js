95a1ba49762702aaa7fdab654117ac98c834143f
Aryan <manglaaryan178@gmail.com>
2021-11-05 22:25:17 +0530

    A

--- index.js
+++ index.js
@@ -2,24 +2,32 @@ const lift = document.querySelector(".lift");
 const btn = document.querySelectorAll("button");
 
 function Lift(e) {
+  var a = e.id;
+  var n = a[5] * 2; 
   switch (e.id) {
     case "floor1":
-      
+      lift.style.transition_duration = "n";
+      lift.style.marginTop = "670px";
       break;
     case "floor2":
-      
+      lift.style.transition_duration = "n";
+      lift.style.marginTop = "520px";
       break;
     case "floor3":
-      
+      lift.style.transition_duration = "n";
+      lift.style.marginTop = "370px";
       break;
     case "floor4":
-      
+      lift.style.transition_duration = "n";
+      lift.style.marginTop = "220px";
       break;
     case "floor5":
-      
+      lift.style.transition_duration = "n";
+      lift.style.marginTop = "70px";
       break;
     case "floor6":
-        lift.style.marginTop = "680px";
+      lift.style.transition_duration = "n";
+      lift.style.marginTop = "-90px";
       break;
   }
 }
