3bdd718433a0377c77f5ce4d2db2c78bb2aefaf6
Aryan <manglaaryan178@gmail.com>
2021-11-05 22:52:31 +0530

    a

--- index.js
+++ index.js
@@ -2,31 +2,29 @@ const lift = document.querySelector(".lift");
 const btn = document.querySelectorAll("button");
 
 function Lift(e) {
-  var a = e.id;
-  var n = a[5] * 2; 
   switch (e.id) {
     case "floor1":
-      lift.style.transition_duration = "n";
+      lift.style.transitionDuration = "2s";
       lift.style.marginTop = "670px";
       break;
     case "floor2":
-      lift.style.transition_duration = "n";
+      lift.style.transitionDuration = "4s";
       lift.style.marginTop = "520px";
       break;
     case "floor3":
-      lift.style.transition_duration = "n";
+      lift.style.transitionDuration = "6s";
       lift.style.marginTop = "370px";
       break;
     case "floor4":
-      lift.style.transition_duration = "n";
+      lift.style.transitionDuration = "8s";
       lift.style.marginTop = "220px";
       break;
     case "floor5":
-      lift.style.transition_duration = "n";
+      lift.style.transitionDuration = "10s";
       lift.style.marginTop = "70px";
       break;
     case "floor6":
-      lift.style.transition_duration = "n";
+      lift.style.transitionDuration = "12s";
       lift.style.marginTop = "-90px";
       break;
   }
