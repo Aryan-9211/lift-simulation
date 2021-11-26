8438a6f2b3258927bd23f18ec7f9b37e6260ba6a
Aryan <manglaaryan178@gmail.com>
2021-11-26 14:01:16 +0530

    fixed animation

--- index.js
+++ index.js
@@ -12,14 +12,14 @@ let j = 1;
 
 function Lift(e) {
   let currheight = parseInt(e.id);
+  let diff = Math.abs(currheight - arr1[i - 1]);
+  let diff2 = Math.abs(currheight - arr2[j - 1]);
+  if (diff2 < diff) {
+    Lift2(e);
+    return;
+  }
   arr1.push(currheight);
   console.log(arr1);
-  let diff = Math.abs(arr1[i] - arr1[i - 1]);
-  // let diff2 = Math.abs(currheight - arr2[j - 1]);
-  // if(diff > diff2){
-  //   Lift2(e);
-  //   return;
-  // }
   let time = diff * 2;
   i++;
   console.log(time);
@@ -30,51 +30,61 @@ function Lift(e) {
     case "1":
       lift.style.transitionDuration = dur;
       lift.style.marginTop = "1280px";
+      doorAnimation();
       removeAnimation();
       break;
     case "2":
       lift.style.transitionDuration = dur;
       lift.style.marginTop = "1130px";
+      doorAnimation();
       removeAnimation();
       break;
     case "3":
       lift.style.transitionDuration = dur;
       lift.style.marginTop = "980px";
+      doorAnimation();
       removeAnimation();
       break;
     case "4":
       lift.style.transitionDuration = dur;
       lift.style.marginTop = "830px";
+      doorAnimation();
       removeAnimation();
       break;
     case "5":
       lift.style.transitionDuration = dur;
       lift.style.marginTop = "680px";
+      doorAnimation();
       removeAnimation();
       break;
     case "6":
       lift.style.transitionDuration = dur;
       lift.style.marginTop = "530px";
+      doorAnimation();
       removeAnimation();
       break;
     case "7":
       lift.style.transitionDuration = dur;
       lift.style.marginTop = "380px";
+      doorAnimation();
       removeAnimation();
       break;
     case "8":
       lift.style.transitionDuration = dur;
       lift.style.marginTop = "230px";
+      doorAnimation();
       removeAnimation();
       break;
     case "9":
       lift.style.transitionDuration = dur;
       lift.style.marginTop = "80px";
+      doorAnimation();
       removeAnimation();
       break;
     case "10":
       lift.style.transitionDuration = dur;
       lift.style.marginTop = "-70px";
+      doorAnimation();
       removeAnimation();
       break;
   }
@@ -94,51 +104,61 @@ function Lift2(e) {
     case "1":
       lift_2.style.transitionDuration = dur;
       lift_2.style.marginTop = "1280px";
+      doorAnimation2();
       removeAnimation();
       break;
     case "2":
       lift_2.style.transitionDuration = dur;
       lift_2.style.marginTop = "1130px";
+      doorAnimation2();
       removeAnimation();
       break;
     case "3":
       lift_2.style.transitionDuration = dur;
       lift_2.style.marginTop = "980px";
+      doorAnimation2();
       removeAnimation();
       break;
     case "4":
       lift_2.style.transitionDuration = dur;
       lift_2.style.marginTop = "830px";
+      doorAnimation2();
       removeAnimation();
       break;
     case "5":
       lift_2.style.transitionDuration = dur;
       lift_2.style.marginTop = "680px";
+      doorAnimation2();
       removeAnimation();
       break;
     case "6":
       lift_2.style.transitionDuration = dur;
       lift_2.style.marginTop = "530px";
+      doorAnimation2();
       removeAnimation();
       break;
     case "7":
       lift_2.style.transitionDuration = dur;
       lift_2.style.marginTop = "380px";
+      doorAnimation2();
       removeAnimation();
       break;
     case "8":
       lift_2.style.transitionDuration = dur;
       lift_2.style.marginTop = "230px";
+      doorAnimation2();
       removeAnimation();
       break;
     case "9":
       lift_2.style.transitionDuration = dur;
       lift_2.style.marginTop = "80px";
+      doorAnimation2();
       removeAnimation();
       break;
     case "10":
       lift_2.style.transitionDuration = dur;
       lift_2.style.marginTop = "-70px";
+      doorAnimation2();
       removeAnimation();
       break;
   }
@@ -150,18 +170,23 @@ btn.forEach((btn) =>
   })
 );
 
-var anime = function doorAnimation() {
-  leftDoor.style.animation = "openingDoor 2.5s 1 .5s";
-  rightDoor.style.animation = "openingDoor1 2.5s 1 .5s";
-  leftDoor2.style.animation = "openingDoor 2.5s 1 .5s";
-  rightDoor2.style.animation = "openingDoor1 2.5s 1 .5s";
+function doorAnimation() {
+  leftDoor.style.animation = `openingDoor 2.5s 1 ${dur}`;
+  rightDoor.style.animation = `openingDoor1 2.5s 1 ${dur}`;
   this.addEventListener("animationend", () => {
-    leftDoor.style.animation = "closingDoor 2.5s 1 0s";
-    rightDoor.style.animation = "closingDoor1 2.5s 1 0s";
+    leftDoor.style.animation = `closingDoor 2.5s 1 0s`;
+    rightDoor.style.animation = `closingDoor1 2.5s 1 0s`;
+  });
+}
+
+function doorAnimation2() {
+  leftDoor2.style.animation = `openingDoor 2.5s 1 ${dur}`;
+  rightDoor2.style.animation = `openingDoor1 2.5s 1 ${dur}`;
+  this.addEventListener("animationend", () => {
     leftDoor2.style.animation = "closingDoor 2.5s 1 0s";
     rightDoor2.style.animation = "closingDoor1 2.5s 1 0s";
   });
-};
+}
 
 function removeAnimation() {
   lift.addEventListener("transitionend", anime);
