090194b0da497540d4aa5bf05601866e58adad51
Aryan <manglaaryan178@gmail.com>
2021-11-26 14:12:24 +0530

    made changes in animation

--- index.js
+++ index.js
@@ -94,69 +94,69 @@ function Lift2(e) {
   let currheight = parseInt(e.id);
   arr2.push(currheight);
   console.log(arr2);
-  let time = Math.abs(arr2[j] - arr2[j - 1]) * 2;
+  let time2 = Math.abs(arr2[j] - arr2[j - 1]) * 2;
   j++;
-  console.log(time);
-  dur = time + "s";
-  console.log(dur);
+  console.log(time2);
+  dur2 = time2 + "s";
+  console.log(dur2);
 
   switch (e.id) {
     case "1":
-      lift_2.style.transitionDuration = dur;
+      lift_2.style.transitionDuration = dur2;
       lift_2.style.marginTop = "1280px";
       doorAnimation2();
       removeAnimation();
       break;
     case "2":
-      lift_2.style.transitionDuration = dur;
+      lift_2.style.transitionDuration = dur2;
       lift_2.style.marginTop = "1130px";
       doorAnimation2();
       removeAnimation();
       break;
     case "3":
-      lift_2.style.transitionDuration = dur;
+      lift_2.style.transitionDuration = dur2;
       lift_2.style.marginTop = "980px";
       doorAnimation2();
       removeAnimation();
       break;
     case "4":
-      lift_2.style.transitionDuration = dur;
+      lift_2.style.transitionDuration = dur2;
       lift_2.style.marginTop = "830px";
       doorAnimation2();
       removeAnimation();
       break;
     case "5":
-      lift_2.style.transitionDuration = dur;
+      lift_2.style.transitionDuration = dur2;
       lift_2.style.marginTop = "680px";
       doorAnimation2();
       removeAnimation();
       break;
     case "6":
-      lift_2.style.transitionDuration = dur;
+      lift_2.style.transitionDuration = dur2;
       lift_2.style.marginTop = "530px";
       doorAnimation2();
       removeAnimation();
       break;
     case "7":
-      lift_2.style.transitionDuration = dur;
+      lift_2.style.transitionDuration = dur2;
       lift_2.style.marginTop = "380px";
       doorAnimation2();
       removeAnimation();
       break;
     case "8":
-      lift_2.style.transitionDuration = dur;
+      lift_2.style.transitionDuration = dur2;
       lift_2.style.marginTop = "230px";
       doorAnimation2();
       removeAnimation();
       break;
     case "9":
-      lift_2.style.transitionDuration = dur;
+      lift_2.style.transitionDuration = dur2;
       lift_2.style.marginTop = "80px";
       doorAnimation2();
       removeAnimation();
       break;
     case "10":
-      lift_2.style.transitionDuration = dur;
+      lift_2.style.transitionDuration = dur2;
       lift_2.style.marginTop = "-70px";
       doorAnimation2();
       removeAnimation();
@@ -180,8 +180,8 @@ function doorAnimation() {
 }
 
 function doorAnimation2() {
-  leftDoor2.style.animation = `openingDoor 2.5s 1 ${dur}`;
-  rightDoor2.style.animation = `openingDoor1 2.5s 1 ${dur}`;
+  leftDoor2.style.animation = `openingDoor 2.5s 1 ${dur2}`;
+  rightDoor2.style.animation = `openingDoor1 2.5s 1 ${dur2}`;
   this.addEventListener("animationend", () => {
     leftDoor2.style.animation = "closingDoor 2.5s 1 0s";
     rightDoor2.style.animation = "closingDoor1 2.5s 1 0s";
